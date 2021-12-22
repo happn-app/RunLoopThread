793e004910c1b0f029939c3e04b1d8cacc99aa1a
Frizlab <fload@me.com>
2021-12-22 12:06:58 +0100

    Reindent everything

--- Sources/RunLoopThread/RunLoopThread.swift
+++ Sources/RunLoopThread/RunLoopThread.swift
@@ -1,11 +1,11 @@
 /*
-Copyright 2020 happn
+Copyright 2020-2021 happn
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,7 +15,7 @@ limitations under the License. */
 
 import Foundation
 #if canImport(os)
-	import os.log
+import os.log
 #endif
 
 
@@ -39,17 +39,17 @@ public final class RunLoopThread : Thread {
 		var ok = true
 		while ok {
 			autoreleasepool{
-				#if canImport(os)
+#if canImport(os)
 				if #available(macOS 10.12, tvOS 10.0, iOS 10.0, watchOS 3.0, *) {
 					RunLoopThreadConfig.oslog.flatMap{ os_log("New RunLoop run for RunLoopThread “%{public}@”", log: $0, type: .debug, String(describing: name)) }}
-				#endif
+#endif
 				ok = (!isCancelled && rl.run(mode: .default, before: .distantFuture))
 			}
 		}
-		#if canImport(os)
+#if canImport(os)
 		if #available(macOS 10.12, tvOS 10.0, iOS 10.0, watchOS 3.0, *) {
 			RunLoopThreadConfig.oslog.flatMap{ os_log("No more RunLoop runs for RunLoopThread “%{public}@”", log: $0, type: .debug, String(describing: name)) }}
-		#endif
+#endif
 	}
 	
 	public override func cancel() {
--- Sources/RunLoopThread/RunLoopThreadConfig.swift
+++ Sources/RunLoopThread/RunLoopThreadConfig.swift
@@ -1,11 +1,11 @@
 /*
-Copyright 2020 happn
+Copyright 2020-2021 happn
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,14 +20,13 @@ import os.log
 
 
 
-/**
-The global configuration for RunLoopThread. */
+/** The global configuration for RunLoopThread. */
 public struct RunLoopThreadConfig {
 	
-	#if canImport(os)
+#if canImport(os)
 	@available(macOS 10.12, tvOS 10.0, iOS 10.0, watchOS 3.0, *)
 	public static var oslog: OSLog? = .default
-	#endif
+#endif
 	
 	/** This struct is simply a container for static configuration properties. */
 	private init() {}
