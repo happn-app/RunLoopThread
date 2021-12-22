e2461c23e2eea2c2e73a7aa8d6058c99d49e7180
Frizlab <fload@me.com>
2021-12-22 12:07:54 +0100

    Move config to enum

--- Sources/RunLoopThread/RunLoopThreadConfig.swift
+++ Sources/RunLoopThread/RunLoopThreadConfig.swift
@@ -21,14 +21,13 @@ import os.log
 
 
 /** The global configuration for RunLoopThread. */
-public struct RunLoopThreadConfig {
+public enum RunLoopThreadConfig {
 	
 #if canImport(os)
 	@available(macOS 10.12, tvOS 10.0, iOS 10.0, watchOS 3.0, *)
 	public static var oslog: OSLog? = .default
 #endif
 	
-	/** This struct is simply a container for static configuration properties. */
-	private init() {}
-	
 }
+
+typealias Conf = RunLoopThreadConfig
